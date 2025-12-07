4b7416a4692d16e0eb4df4ed5011a56417879c23
kgraph57 <207868202+kgraph57@users.noreply.github.com>
2025-12-06 19:25:17 -0500

    fix: Upgrade cache busting to v3 for Tips page

--- vite.config.ts
+++ vite.config.ts
@@ -25,9 +25,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
-        assetFileNames: `assets/[name]-[hash]-v2.[ext]`
+        entryFileNames: `assets/[name]-[hash]-v3.js`,
+        chunkFileNames: `assets/[name]-[hash]-v3.js`,
+        assetFileNames: `assets/[name]-[hash]-v3.[ext]`
       }
     }
   },
